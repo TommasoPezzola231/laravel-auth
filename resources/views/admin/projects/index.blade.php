fd8743f781e955b629130bc1aae8a8301fdcb00e
TommasoPezzola <pezzola123@gmail.com>
2023-07-10 19:15:28 +0200

    feat,style: add show project

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -6,13 +6,15 @@
         <div class="row">
             <a class="btn btn-primary my-2" href="{{ route("admin.projects.create") }}">Aggiungi</a>
             @foreach ($projects as $project)
-                <div class="card col-3 m-2" style="width: 18rem;">
-                    <img src="{{ ($project->image) ? $project->image : "https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png" }}" class="card-img-top" alt="{{ $project->title }}">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $project->title }}</h5>
-                        <p class="card-text">{{ $project->content }}</p>
+                <a class="col-3" href="{{ route("admin.projects.show", $project->id) }}">
+                    <div class="card col-4 m-2" style="width: 18rem;">
+                        <img src="{{ ($project->image) ? $project->image : "https://www.signfix.com.au/wp-content/uploads/2017/09/placeholder-600x400.png" }}" class="card-img-top" alt="{{ $project->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $project->title }}</h5>
+                            <p class="card-text">{{ $project->content }}</p>
+                        </div>
                     </div>
-                </div>
+                </a>
             @endforeach
         </div>
     </section>
